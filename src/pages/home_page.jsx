d0ecfc0af0cec8edeac98e0b3919185e2880a610
doorkaz <retamalespj@gmail.com>
2024-10-30 02:43:33 -0300

    Configuración de Linter StandardJS y formatteo de .jsx

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function HomePage() {
+function HomePage () {
   return (
     <>
       <h3>Levantamiento del proyecto</h3>
     </>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
