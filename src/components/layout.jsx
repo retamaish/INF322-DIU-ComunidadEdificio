2783a9c34b1b00550a9fc84ea97491beb79197e8
doorkaz <retamalespj@gmail.com>
2024-11-02 00:51:06 -0300

    Página Login seteada en rutas y avance inicial.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import HomePage from '../pages/home_page'
+import { Login } from '../pages/Login'
 
 const Layout = () => {
   return (
     <BrowserRouter>
       <div className='layout'>
-        <h1 className='layout__title'>Comunidad Edificio</h1>
         <div className='layout__page'>
           <Routes>
-            <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<Login />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { H1, H2, P } from '@components'
+
+export const Login = () => {
+  return (
+    <div className='login'>
+      <H1 className='login__title'>Bienvenido a Comunidad Edificio</H1>
+      <H2 className='login__subtitle'>Ingresa tus datos</H2>
+
+      <form className='form'>
+        {/* Correo */}
+        <div className='form__group'>
+          <label className='form__label' for='email'>Correo electrónico</label>
+          <input className='form__input' type='email' id='email' pattern='.+@example\.com' size='30' autoComplete={1} placeholder='johndoe@gmail.com' required />
+        </div>
+
+        {/* Contraseña */}
+        <div className='form__group' style={{ marginTop: '36px' }}>
+          <label className='form__label' for='password'>Contraseña</label>
+          <input className='form__input' type='password' id='password' name='password' placeholder='******' required />
+        </div>
+
+        <button style={{ marginTop: '36px' }} type='submit' className='button button__submit'>Entrar</button>
+      </form>
+      <a>Olvidaste tu contraseña?</a>
+      <P>¿No tienes cuenta? <a>Regístrate</a></P>
+    </div>
+  )
+}
