e3be5fb1e0f24da08763a3aa560d11d2c0d5bdb0
doorkaz <retamalespj@gmail.com>
2024-11-04 19:16:26 -0300

    Estandarización de español a código en ingles [RegisterVisitors.jsx]

--- src/pages/RegisterVisitors.jsx
+++ src/pages/RegisterVisitors.jsx
@@ -3,12 +3,13 @@ import { H2 } from '@components'
 
 export const RegisterVisitors = () => {
   return (
-    <div className='visitas_containerAll'>
+    <div className='register-visitors'>
       <h1 style={{ justifyContent: 'center' }}>Inscribe tu Visita</h1>
-      <div className='visitas-container' style={{ display: 'flex', gap: '2rem', justifyContent: 'center' }}>
+      <div className='register-visitors__container' style={{ display: 'flex', gap: '2rem', justifyContent: 'center' }}>
         {/* formulario ingreso nuevo visitante */}
-        <div className='visita'>
-          <H2 className='visita__subtitle'>Agendar Visitante</H2>
+        <div className='register-visitors'>
+          <H2 className='register-visitors__subtitle'>Agendar Visitante</H2>
+
           <form className='form'>
             {/* Rut */}
             <div className='form__group'>
@@ -17,22 +18,22 @@ export const RegisterVisitors = () => {
             </div>
             {/* Nombre */}
             <div className='form__group' style={{ marginTop: '24px' }}>
-              <label className='form__label' htmlFor='nombre'>Nombre</label>
-              <input className='form__input' type='text' id='nombre' name='nombre' placeholder='Nombre' required />
+              <label className='form__label' htmlFor='name'>Nombre</label>
+              <input className='form__input' type='text' id='name' name='name' placeholder='Nombre' required />
             </div>
             {/* Apellidos */}
             <div className='form__group' style={{ marginTop: '24px' }}>
-              <label className='form__label' htmlFor='apellidos'>Apellidos</label>
-              <input className='form__input' type='text' id='apellidos' name='apellidos' placeholder='Apellidos' required />
+              <label className='form__label' htmlFor='lastName'>Apellidos</label>
+              <input className='form__input' type='text' id='lastName' name='lastName' placeholder='Apellidos' required />
             </div>
-            <label><input style={{ marginTop: '24px' }} type='radio' name='opcion' value='opcion1' />¿Guardar como visitante frecuente?</label>
+            <label><input style={{ marginTop: '24px' }} type='radio' name='option' value='option1' />¿Guardar como visitante frecuente?</label>
             <button style={{ marginTop: '24px' }} type='submit' className='button button__submit'>Entrar</button>
           </form>
         </div>
 
         {/* VISITANTES REGISTRADOS */}
-        <div className='visita'>
-          <H2 className='visita__subtitle'>Visitantes registrados:</H2>
+        <div className='visitors'>
+          <H2 className='visitors__subtitle'>Visitantes registrados:</H2>
           <form className='form'>
             <button style={{ marginTop: '24px' }} type='submit' className='button button__submit'>Volver al inicio</button>
           </form>
