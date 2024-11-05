ab237f8ed3899f0fd0f4a0d0fc2b9ea3eed5aa2e
doorkaz <retamalespj@gmail.com>
2024-11-04 22:19:09 -0300

    AccountStatement actualizado

--- src/pages/AccountStatement.jsx
+++ src/pages/AccountStatement.jsx
@@ -1,32 +1,18 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { IconDownload } from '@icons';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { IconDownload } from '@icons'
 
 export const AccountStatement = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleDownload = (type) => {
-    alert(`Descarga realizada: ${type}`);
-  };
+    alert(`Descarga realizada: ${type}`)
+  }
 
   return (
-    <div className="cartola-container">
-      <h1 className="cartola-title">Cartola</h1>
-      <div className="download-buttons">
-        <button
-          className="download-button"
-          onClick={() => handleDownload("PDF")}
-        >
-          Descargar PDF <IconDownload className="icon" />
-        </button>
-        <button
-          className="download-button"
-          onClick={() => handleDownload("Excel")}
-        >
-          Descargar Excel <IconDownload className="icon" />
-        </button>
-      </div>
-      <table className="cartola-table">
+    <div className='account-statement__container'>
+      <h1 className='account-statement__title'>Cartola</h1>
+      <table className='account-statement__table'>
         <thead>
           <tr>
             <th>Fecha Ingreso</th>
@@ -41,59 +27,72 @@ export const AccountStatement = () => {
           <tr>
             <td>5/08/24</td>
             <td>120.000</td>
-            <td></td>
+            <td />
             <td>Gasto común, Agosto</td>
             <td>120.000</td>
-            <td className="document-icon-cell">
-              <IconDownload className="icon" aria-label="Descargar Documento" />
+            <td className='document-icon-cell'>
+              <IconDownload className='icon' aria-label='Descargar Documento' />
             </td>
           </tr>
           <tr>
             <td>5/08/24</td>
             <td>120.000</td>
-            <td></td>
+            <td />
             <td>Gasto común, Agosto</td>
             <td>120.000</td>
-            <td className="document-icon-cell">
-              <IconDownload className="icon" aria-label="Descargar Documento" />
+            <td className='document-icon-cell'>
+              <IconDownload className='icon' aria-label='Descargar Documento' />
             </td>
           </tr>
           <tr>
             <td>5/08/24</td>
             <td>120.000</td>
-            <td></td>
+            <td />
             <td>Gasto común, Agosto</td>
             <td>120.000</td>
-            <td className="document-icon-cell">
-              <IconDownload className="icon" aria-label="Descargar Documento" />
+            <td className='document-icon-cell'>
+              <IconDownload className='icon' aria-label='Descargar Documento' />
             </td>
           </tr>
           <tr>
             <td>5/08/24</td>
             <td>120.000</td>
-            <td></td>
+            <td />
             <td>Gasto común, Agosto</td>
             <td>120.000</td>
-            <td className="document-icon-cell">
-              <IconDownload className="icon" aria-label="Descargar Documento" />
+            <td className='document-icon-cell'>
+              <IconDownload className='icon' aria-label='Descargar Documento' />
             </td>
           </tr>
           <tr>
             <td>5/08/24</td>
             <td>120.000</td>
-            <td></td>
+            <td />
             <td>Gasto común, Agosto</td>
             <td>120.000</td>
-            <td className="document-icon-cell">
-              <IconDownload className="icon" aria-label="Descargar Documento" />
+            <td className='document-icon-cell'>
+              <IconDownload className='icon' aria-label='Descargar Documento' />
             </td>
           </tr>
         </tbody>
       </table>
-      <button className="back-button" onClick={() => navigate("/home")}>
-        Volver a Inicio
-      </button>
+      <div className='download-buttons'>
+        <button
+          className='download-button'
+          onClick={() => handleDownload('PDF')}
+        >
+          Descargar PDF <IconDownload className='icon' />
+        </button>
+        <button
+          className='download-button'
+          onClick={() => handleDownload('Excel')}
+        >
+          Descargar Excel <IconDownload className='icon' />
+        </button>
+        <button className='download-button' onClick={() => navigate('/iniciar-sesion')}>
+          Volver a Inicio
+        </button>
+      </div>
     </div>
-  );
-};
-
+  )
+}
